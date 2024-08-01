bc9ff7a04f9460b9253cd51b46053555f9839be9
Umesha Rukshan Silva <umesharukshan2000@gmail.com>
2024-08-01 22:59:49 +0530

    gg

--- BACKEND/routes/generateSecretKey.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const generateSecretKey = () => {
-  const secretKey = crypto.randomBytes(32).toString('hex');
-  console.log(`Generated Secret Key: ${secretKey}`);
-};
-
-generateSecretKey();
